b788b1cb56534906dbc83a4e230274d7acd7d42e
rhydiaan <rhydian.davies01@gmail.com>
2024-01-05 13:51:50 +0000

    feat: prettified grid and added basic banker algorithm

--- src/app/components/banker/banker.component.ts
+++ src/app/components/banker/banker.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class BankerComponent {
 
+  mean: number = 20000;
+  maxNumber: number = 100000;
+  casesRemaining: number = 11;
+  offer = (12275.30 + (0.748 * this.mean) + (2714.74 * this.casesRemaining) + (0.40 * this.maxNumber) + (.0000006986 * this.mean^2) + (32.623 * this.casesRemaining^2)) / 4
+
 }
